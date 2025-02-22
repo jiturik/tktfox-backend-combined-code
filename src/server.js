04b9fb6f71f6c5186d61cd1fc8a6cd30556f8572
jiturik <jiturikhadi@gmail.com>
2025-02-22 16:40:32 +0530

    error

--- src/server.js
+++ src/server.js
@@ -1,35 +1,35 @@
-import http from 'http';
-import express, { Router } from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import helmet from 'helmet';
-import { jwtDecode } from 'jwt-decode';
-import jwt_token from 'jsonwebtoken';
-import winston from 'winston';
-import path, { dirname } from 'path';
-import fs from 'fs';
-import { v4 } from 'uuid';
-import bcrypt from 'bcryptjs';
-import moment$1 from 'moment';
-import nodemailer from 'nodemailer';
-import momentTimeZone from 'moment-timezone';
-import _ from 'lodash';
-import NodeCache from 'node-cache';
-import zlib from 'zlib';
-import multer from 'multer';
-import excel from 'exceljs';
-import ejs from 'ejs';
-import pdf from 'html-pdf';
-import QRCode from 'qrcode';
-import { SeatsioClient, Region } from 'seatsio';
-import { createHash } from 'crypto';
-import axios$1 from 'axios';
-import { fileURLToPath } from 'url';
-import { attachPaginate } from 'knex-paginate';
-import { KnexConnection } from './knex/knex.js';
-import Redis from 'ioredis';
-import 'knex';
-import 'dotenv';
+import http from "http";
+import express, { Router } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import { jwtDecode } from "jwt-decode";
+import jwt_token from "jsonwebtoken";
+import winston from "winston";
+import path, { dirname } from "path";
+import fs from "fs";
+import { v4 } from "uuid";
+import bcrypt from "bcryptjs";
+import moment$1 from "moment";
+import nodemailer from "nodemailer";
+import momentTimeZone from "moment-timezone";
+import _ from "lodash";
+import NodeCache from "node-cache";
+import zlib from "zlib";
+import multer from "multer";
+import excel from "exceljs";
+import ejs from "ejs";
+import pdf from "html-pdf";
+import QRCode from "qrcode";
+import { SeatsioClient, Region } from "seatsio";
+import { createHash } from "crypto";
+import axios$1 from "axios";
+import { fileURLToPath } from "url";
+import { attachPaginate } from "knex-paginate";
+import { KnexConnection } from "./knex/knex.js";
+import Redis from "ioredis";
+import "knex";
+import "dotenv";
 
 // Ensure the logs directory exists
 const logsDir = path.resolve("src/winston-logs");
@@ -586,7 +586,7 @@ function sendEmailClient(from, subject, body, attachment) {
       html: body,
     };
 
-    if (attachment) ;
+    if (attachment);
 
     mail.sendMail(mailOptions, (error, info) => {
       if (error) {
@@ -1550,12 +1550,9 @@ async function getActiveEventList(req, res) {
     const data = await getActiveListData(reqbody);
 
     await storeInRedis("redisCache:activeWebsiteEventList", data, 3600);
-    return sendResponse(
-      res,
-      200,
-      "Active event list fetched successfully",
-      data
-    );
+    return sendResponse(res, 200, "Active event list fetched successfully", {
+      data,
+    });
   } catch (error) {
     return sendResponse(
       res,
@@ -2379,7 +2376,11 @@ function ScannerRoutes() {
     checkSessionExist,
     getScannedTicketById
   );
-  router$8.get("/getScannedTicketList", checkSessionExist, getScannedTicketList);
+  router$8.get(
+    "/getScannedTicketList",
+    checkSessionExist,
+    getScannedTicketList
+  );
 
   // POST Routes
   router$8.post(
@@ -4736,11 +4737,7 @@ var opts = {
   },
 };
 
-async function createQRCode(
-  qrcode_data,
-  returnType = "buffer",
-  logo = null
-) {
+async function createQRCode(qrcode_data, returnType = "buffer", logo = null) {
   return new Promise(async (resolve, reject) => {
     try {
       if (!qrcode_data) {
@@ -4824,7 +4821,10 @@ const CreateInvSendTicketEmail = async (reqbody) => {
             console.log("QR Code generated:", booking.booking_code);
           })
           .catch((error) => {
-            winstonLogger$1.error("Error in CreateInvSendTicketEmail 1:", error);
+            winstonLogger$1.error(
+              "Error in CreateInvSendTicketEmail 1:",
+              error
+            );
             console.error("error in qr generation", error.message);
           });
 
@@ -4991,9 +4991,9 @@ const createInvoicePdf = async (emailData) => {
   });
 };
 
-var CreateInvSendTicketEmail$1 = /*#__PURE__*/Object.freeze({
+var CreateInvSendTicketEmail$1 = /*#__PURE__*/ Object.freeze({
   __proto__: null,
-  CreateInvSendTicketEmail: CreateInvSendTicketEmail
+  CreateInvSendTicketEmail: CreateInvSendTicketEmail,
 });
 
 async function getTransactionList(req, res) {
@@ -5686,7 +5686,11 @@ function ReportRoutes() {
     checkSessionExist,
     getReservationBookingList
   );
-  router$4.get("/getEventHomeDataById", checkSessionExist, getEventHomeDataById);
+  router$4.get(
+    "/getEventHomeDataById",
+    checkSessionExist,
+    getEventHomeDataById
+  );
   router$4.get("/exportBookingReport", checkSessionExist, exportBookingReport);
   router$4.get(
     "/exportReservationReport",
@@ -6939,11 +6943,15 @@ const getReservationSeat = async (req, res) => {
 
     // Calculate time difference for reservation release
     obj.seconds =
-      moment$1(obj.release_time).diff(moment$1(obj.currentDateTime), "seconds") %
-      60;
+      moment$1(obj.release_time).diff(
+        moment$1(obj.currentDateTime),
+        "seconds"
+      ) % 60;
     obj.minutes =
-      moment$1(obj.release_time).diff(moment$1(obj.currentDateTime), "minutes") %
-      60;
+      moment$1(obj.release_time).diff(
+        moment$1(obj.currentDateTime),
+        "minutes"
+      ) % 60;
 
     // add voucher discount details in response if available
     if (getReservationDetail[0].voucher_applied == "Y") {
@@ -7610,11 +7618,15 @@ const getReservePassDetails = async (req, res) => {
 
     // Calculate time remaining for release
     obj.seconds =
-      moment$1(obj.release_time).diff(moment$1(obj.currentDateTime), "seconds") %
-      60;
+      moment$1(obj.release_time).diff(
+        moment$1(obj.currentDateTime),
+        "seconds"
+      ) % 60;
     obj.minutes =
-      moment$1(obj.release_time).diff(moment$1(obj.currentDateTime), "minutes") %
-      60;
+      moment$1(obj.release_time).diff(
+        moment$1(obj.currentDateTime),
+        "minutes"
+      ) % 60;
 
     // Format pass validity date
     obj.pass_validity_to = moment$1()
@@ -8068,7 +8080,11 @@ function WebsiteRoutes() {
 
   // POST Routes
   router$2.post("/signup-customer", checkWebsiteSessionExist, addWebCustomer);
-  router$2.post("/verify-otp", checkWebsiteSessionExist, verifyOTPAndUpdateUser);
+  router$2.post(
+    "/verify-otp",
+    checkWebsiteSessionExist,
+    verifyOTPAndUpdateUser
+  );
 
   router$2.post("/signIn", checkWebsiteSessionExist, customerSignIn);
 
@@ -10258,7 +10274,7 @@ Promise.all([KnexConnection()])
       });
 
     //cron scripts
-    import('./index-B8jbhj46.js');
+    import("./index-B8jbhj46.js");
 
     //start server
     httpServer.listen(EXPRESS_PORT, () => {
